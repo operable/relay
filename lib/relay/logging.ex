0cc6315dc147bbf19014547c21651077aa4377d4
Kevin Smith <kevin@operable.io>
2015-12-13 12:09:05 -0500

    Helper to add consistency to progress logging statements

---
+++ lib/relay/logging.ex
@@ -0,0 +1,18 @@
+defmodule Relay.Logging do
+
+  defmacro __using__(_) do
+    quote do
+      require Logger
+      import unquote(__MODULE__), only: [ready: 1]
+    end
+  end
+
+  defmacro ready(value) do
+    caller = __CALLER__.module
+    quote bind_quoted: [caller: caller, value: value] do
+      Logger.info("#{caller} ready...")
+      value
+    end
+  end
+
+end
