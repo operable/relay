7ddb2d6058a1f6ae235b586a0f3ab3241797a8d5
Kevin Smith <kevin@operable.io>
2015-12-14 14:13:14 -0500

    Remove dead code

--- lib/relay/bundle/bundle_sup.ex
+++ lib/relay/bundle/bundle_sup.ex
@@ -10,8 +10,6 @@ defmodule Relay.Bundle.BundleSup do
     do: Supervisor.start_link(__MODULE__, [], name: __MODULE__)
 
   def init([]) do
-    # children = [supervisor(Relay.Bundle.BundleCatalog, []),
-    #             worker(Relay.Bundle.BundleLoader, [])]
     children = [worker(Relay.Bundle.BundleCatalog, []),
                 worker(Relay.Bundle.Scanner, [])]
     ready(supervise(children, strategy: :rest_for_one, max_restarts: 5, max_seconds: 60))
