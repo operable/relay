2dce5640a4a7976806b030550e0a6a7cead15aff
Christopher Maier <chris@operable.io>
2015-12-30 20:59:54 -0500

    Rework Announcer as an FSM
    To be more robust in the face of network issues, the Announcer has
    been converted from a server to an FSM. The overall scheme of the
    state machine is:
    
        introduction -> snapshot announcement -> ongoing incremental announcements
    
    Announcements happen strictly in that order after startup, in order to
    ensure the bot has a consistent picture of what the Relay offers.
    
    Announcements are also periodically re-sent until the bot sends back
    an acknowledgment message. This ensures that no messages get lost,
    further ensuring consistent state everywhere.
    
    Additionally, existing bundle startup has been moved out of the
    Announcer and into a Starter server. Admittedly, this is perhaps
    overkill, but it ensures Single Responsibility and places startup into
    the overall supervision tree in the appropriate place.

---
+++ lib/relay/bundle/starter.ex
@@ -0,0 +1,33 @@
+defmodule Relay.Bundle.Starter do
+  @moduledoc """
+  Process that exists solely to start up all currently-recognized
+  bundles at system start-up.
+  """
+
+  # GenServer might be a bit overkill for this, admittedly.
+  use GenServer
+  use Adz
+
+  alias Relay.Bundle.Catalog
+  alias Relay.Bundle.Runner
+
+  def start_link(),
+    do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
+
+  def init([]) do
+    Logger.info("Starting installed command bundles")
+    for bundle <- Catalog.list_bundles() do
+      installed_path = Catalog.installed_path(bundle)
+      commands = Catalog.list_commands(bundle)
+      case Runner.start_bundle(bundle, installed_path, commands) do
+        {:ok, _} ->
+          Logger.info("Bundle #{bundle} started")
+        {:error, reason} ->
+          Logger.error("Error starting bundle #{bundle}: #{inspect reason}")
+      end
+    end
+    # TODO: error out instead of anything failed to come up?
+    :ignore
+  end
+
+end
