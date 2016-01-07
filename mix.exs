70aa3eb9ac6132e69fee51de264de684033c3cad
vanstee <patrick@vanstee.me>
2016-01-07 12:02:55 -0500

    Bump spanner

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule Relay.Mixfile do
      #
      # Ditto for Piper (a dependency of spanner and runtime dependency
      # of bundles).
-     {:spanner, git: "git@github.com:operable/spanner", ref: "5c11a2316e27b86ae7857e67d35dc56a0e20b7c2"},
+     {:spanner, git: "git@github.com:operable/spanner", ref: "4b38ada8203bc9fb86bfe963e60040b9d6980b53"},
 
      # Same as Loop uses, and only for test, as a way to get around
      # Mix's annoying habit of starting up the application before
