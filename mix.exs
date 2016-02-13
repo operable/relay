260313d26d31a99fa5b27cdaf405ddf000717a77
Kevin Smith <kevin@operable.io>
2016-02-13 13:38:24 -0500

    bumping Spanner dep

--- mix.exs
+++ mix.exs
@@ -61,7 +61,7 @@ defmodule Relay.Mixfile do
      #
      # Ditto for Piper (a dependency of spanner and runtime dependency
      # of bundles).
-     {:spanner, git: "git@github.com:operable/spanner", ref: "265a5bd21cd7c296f317dd0675dfc1255680438f"},
+     {:spanner, git: "git@github.com:operable/spanner", ref: "779627c54b377f13bf62400efd50c5fcbad9fb42"},
      # Same as Cog uses, and only for test, as a way to get around
      # Mix's annoying habit of starting up the application before
      # running ExUnit; Relay will not start unless there is a message
